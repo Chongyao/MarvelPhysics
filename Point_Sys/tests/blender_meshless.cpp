bc6bea92ae976e5bdcaed42356723578d37edcff
cyzhao <471038339@qq.com>
2019-02-01 04:22:14 +0800

    integrate explicit and implicit method

--- Point_Sys/tests/blender_meshless.cpp
+++ Point_Sys/tests/blender_meshless.cpp
@@ -31,7 +31,7 @@ using namespace chrono;
 using namespace boost;
 
 int main(int argc, char** argv){
-  cout.precision(20);
+
   Eigen::initParallel();
   cout << "[INFO]>>>>>>>>>>>>>>>>>>>Eigen parallel<<<<<<<<<<<<<<<<<<" << endl;
   cout << "enable parallel in Eigen in " << nbThreads() << " threads" << endl;
@@ -80,7 +80,7 @@ int main(int argc, char** argv){
   gen_points(nods, surf, simulation_para.get<size_t>("num_in_axis"), points, true);
   cout << points.rows() << " " << points.cols() << endl;
   // #if 1
- // points = nods;
+  // points = nods;
   // #endif
   size_t dim = points.cols();
   cout <<"generate points done." << endl;
@@ -99,7 +99,7 @@ int main(int argc, char** argv){
 
   //get friends of every point
   vector<vector<size_t>> friends_all(dim);
-// #pragma omp parallel for
+  // #pragma omp parallel for
   for(size_t i = 0; i < dim; ++i){
     SH.get_friends(points.col(i), sup_radi(i), friends_all[i]);
   }
@@ -115,22 +115,28 @@ int main(int argc, char** argv){
   //This should read from file. We loop for some points to restrain here.
   //Constraints vary from different models and situations.
   vector<size_t> cons(0);
+  auto cons_file_path = indir + "/" +  mesh_name +".csv";
+
+  if ( boost::filesystem::exists(cons_file_path) )
+    read_fixed_verts_from_csv(cons_file_path.c_str(), cons);
+  cout << "constrint " << cons.size() << " points" << endl;
 
   position_constraint pos_cons(simulation_para.get<double>("position_weig"), cons, dim);
-  
+
   cout << "[INFO]>>>>>>>>>>>>>>>>>>>Gravity<<<<<<<<<<<<<<<<<<" << endl;
   double gravity = common.get<double>("gravity");
-  gravity_energy GE(simulation_para.get<double>("w_g"), gravity, dim, PS.get_Mass_VectorXd(), 'z');
+  gravity_energy GE(simulation_para.get<double>("w_g"), gravity, dim, PS.get_Mass_VectorXd(), 'y');
 
   
   cout << "[INFO]>>>>>>>>>>>>>>>>>>>COLLISION<<<<<<<<<<<<<<<<<<" << endl;
-  collision COLL(simulation_para.get<double>("w_coll"),'z', simulation_para.get<double>("g_pos"), nods.cols(), dim);
+  collision COLL(simulation_para.get<double>("w_coll"),'y', simulation_para.get<double>("g_pos"), nods.cols(), dim);
 
   
   cout << "[INFO]>>>>>>>>>>>>>>>>>>>SOlVE<<<<<<<<<<<<<<<<<<" << endl;
   //initilize variables in time integration
   energy_dat dat_str (dim);
 
+  string solver = simulation_para.get<string>("solver");
 
   double delt_t = common.get<double>("time_step");
   MatrixXd displace;
@@ -154,55 +160,155 @@ int main(int argc, char** argv){
   double previous_step_Val = 0;
 
   auto start = system_clock::now();
-  for(size_t i = 0; i < max_iter; ++i){
-    cerr << "iter is "<<endl<< i << endl;
-    cout << "displace is " << endl<< displace.block(0, 0, 3, 7) << endl;
-    cout << "velocity is "<<endl<< velocity.block(0, 0, 3, 7) << endl;
-    // cout << "acce is " << endl << acce.block(0, 0, 3, 8) << endl;
+  if(solver == "explicit"){
+    for(size_t i = 0; i < max_iter; ++i){
+      cerr << "iter is "<<endl<< i << endl;
+      cout << "displace is " << endl<< displace.block(0, 0, 3, 7) << endl;
+      cout << "velocity is "<<endl<< velocity.block(0, 0, 3, 7) << endl;
+      // cout << "acce is " << endl << acce.block(0, 0, 3, 8) << endl;
 
 
-    GE.Val(displace.data(), dat_str);
-    GE.Gra(displace.data(), dat_str);
+      GE.Val(displace.data(), dat_str);
+      GE.Gra(displace.data(), dat_str);
 
-    PS.Val(displace.data(), dat_str);
-    PS.Gra(displace.data(), dat_str);
+      PS.Val(displace.data(), dat_str);
+      PS.Gra(displace.data(), dat_str);
 
-    COLL.Val(nods.data(), displace.data(), dat_str);
-    COLL.Gra(nods.data(), displace.data(), dat_str, PS.get_Mass_VectorXd());
+      COLL.Val(points.data(), displace.data(), dat_str);
+      COLL.Gra(points.data(), displace.data(), dat_str, PS.get_Mass_VectorXd());
+      pos_cons.Gra(displace.data(), dat_str);
+      pos_cons.Hes(displace.data(),dat_str);
+         
 
 
 
 
-    for(size_t j = 0; j < dim; ++j){
-      assert(PS.get_mass(j) > 0);
-      new_acce.col(j) = dat_str.gra_.col(j)/PS.get_mass(j) - velocity.col(j)*dump;
+      for(size_t j = 0; j < dim; ++j){
+        assert(PS.get_mass(j) > 0);
+        new_acce.col(j) = dat_str.gra_.col(j)/PS.get_mass(j) - velocity.col(j)*dump;
+      }
+    
+      velocity += delt_t * new_acce;
+      displace += delt_t *velocity;
+
+      if (i > 10 && fabs(dat_str.Val_ - previous_step_Val) < 1e-6)
+        break;
+    
+      previous_step_Val = dat_str.Val_;
+    
+      acce = new_acce;
+      
+      if(i%iters_perframe == 0){
+        auto surf_filename = outdir  + "/" + mesh_name + "_" + to_string(i) + ".obj";
+        auto point_filename = outdir + "/" + mesh_name + "_points_" + to_string(i) + ".vtk";
+        MatrixXd points_now = points + displace;
+        point_write_to_vtk(point_filename.c_str(), points_now.data(), dim);
+        point_vector_append2vtk(false, point_filename.c_str(), velocity, dim, "velocity");
+        point_vector_append2vtk(true, point_filename.c_str(), acce, dim, "accelarate");
+        point_scalar_append2vtk(true, point_filename.c_str(), dat_str.ela_val_, dim, "strain_Energy");
+        point_scalar_append2vtk(true, point_filename.c_str(), dat_str.vol_val_, dim, "vol_conservation_Energy");
+
+        vet_displace = displace.block(0, 0, 3, nods.cols());
+        writeOBJ(surf_filename.c_str(), (nods + vet_displace).transpose(), surf.transpose());
+
+      }
+
+      dat_str.set_zero();
     }
-    
-    velocity += delt_t * new_acce;
-    displace += delt_t *velocity;
+  }
+  else{
+    SparseMatrix<double> A_CG(dim * 3, dim * 3);
+    VectorXd b_CG(dim * 3);
+    SparseMatrix<double> M = PS.get_Mass_Matrix();
 
-    if (i > 10 && fabs(dat_str.Val_ - previous_step_Val) < 1e-6)
-      break;
-    
-    previous_step_Val = dat_str.Val_;
-    
-    acce = new_acce;
-    if(i%iters_perframe == 0){
-      auto surf_filename = outdir  + "/" + mesh_name + "_" + to_string(i) + ".obj";
-      auto point_filename = outdir + "/" + mesh_name + "_points_" + to_string(i) + ".vtk";
-      MatrixXd points_now = points + displace;
-      point_write_to_vtk(point_filename.c_str(), points_now.data(), dim);
-      point_vector_append2vtk(false, point_filename.c_str(), velocity, dim, "velocity");
-      point_vector_append2vtk(true, point_filename.c_str(), acce, dim, "accelarate");
-      point_scalar_append2vtk(true, point_filename.c_str(), dat_str.ela_val_, dim, "strain_Energy");
-      point_scalar_append2vtk(true, point_filename.c_str(), dat_str.vol_val_, dim, "vol_conservation_Energy");
+    for(size_t i = 0; i < max_iter; ++i){
+      cout << "iter is "<<endl<< i << endl;
+      cout << "displace is " << endl<< displace.block(0, 0, 3, 8) << endl;
+      // cout << "velocity is "<<endl<< velocity.block(0, 0, 3, 8) << endl;
 
-      vet_displace = displace.block(0, 0, 3, nods.cols());
-      writeOBJ(surf_filename.c_str(), (nods + vet_displace).transpose(), surf.transpose());
+      //newtown iter
+      auto displace_plus = displace;
+      Map<VectorXd> disp_t_plus(displace_plus.data(), 3*dim);
+      Map<VectorXd> disp_t(displace.data(), 3*dim);
+      Map<VectorXd> velo_t(velocity.data(), 3*dim);
+      Map<VectorXd> _F(dat_str.gra_.data(), 3*dim);
 
+
+      for(size_t newton_i = 0; newton_i < 999; ++newton_i){
+        cout << "newton iter " << newton_i << endl;
+      
+
+    
+        PS.Val(displace_plus.data(), dat_str);
+        PS.Gra(displace_plus.data(), dat_str);
+        PS.Hessian(displace_plus.data(), dat_str);
+
+        GE.Val(displace_plus.data(), dat_str);
+        GE.Gra(displace_plus.data(), dat_str);
+
+        pos_cons.Gra(displace_plus.data(), dat_str);
+        pos_cons.Hes(displace_plus.data(),dat_str);
+
+        COLL.Val(points.data(), displace_plus.data(), dat_str);
+        COLL.Gra(points.data(), displace_plus.data(), dat_str, PS.get_Mass_VectorXd());
+              
+
+        //test  convergence
+        auto res = M * ((disp_t_plus - disp_t) / delt_t - velo_t) - delt_t * _F;
+
+        double res_value = res.array().square().sum();
+        if(res_value < 1e-10){
+          cout << "[INFO]Newton res " <<endl << res_value << endl;;
+          cout << "[INFO]>>>>>>>>>>>>>>>>>>>Elasticity Energy Val<<<<<<<<<<<<<<<<<<" << endl;
+          cout << dat_str.Val_ << endl;
+          cout << "[INFO]>>>>>>>>>>>>>>>>>>>GRA<<<<<<<<<<<<<<<<<<" << endl;
+          cout << dat_str.gra_.array().square().sum() << endl;
+          cout << endl<<endl;
+          break;
+        }
+      
+      
+
+    
+        //implicit time integral
+    
+        A_CG.setZero();
+        dat_str.hes_.setFromTriplets(dat_str.hes_trips.begin(), dat_str.hes_trips.end());
+    
+    
+        A_CG = M + delt_t*delt_t*dat_str.hes_;
+        b_CG = M * (delt_t * velo_t + disp_t - disp_t_plus) + delt_t * delt_t * _F;  
+    
+      
+        cout << "[INFO]>>>>>>>>>>>>>>>>>>>A_CG<<<<<<<<<<<<<<<<<<" << endl;          ConjugateGradient<SparseMatrix<double>, Lower|Upper> cg;
+        cg.setMaxIterations(3*dim);
+        cg.setTolerance(1e-8);
+        cg.compute(A_CG);
+        disp_t_plus += cg.solve(b_CG);
+        dat_str.set_zero();
+        cout << "#iterations:     " << cg.iterations() << endl;
+        cout << "estimated error: " << cg.error()      << endl;
+
+      }
+    
+    
+      velocity = (displace_plus - displace)/delt_t;
+      displace = displace_plus;
+      if(i%iters_perframe == 0){
+        auto surf_filename = outdir  + "/" + mesh_name + "_" + to_string(i) + ".obj";
+        auto point_filename = outdir + "/" + mesh_name + "_points_" + to_string(i) + ".vtk";
+        MatrixXd points_now = points + displace;
+        point_write_to_vtk(point_filename.c_str(), points_now.data(), dim);
+        point_vector_append2vtk(false, point_filename.c_str(), velocity, dim, "velocity");
+        point_vector_append2vtk(true, point_filename.c_str(), acce, dim, "accelarate");
+        point_scalar_append2vtk(true, point_filename.c_str(), dat_str.ela_val_, dim, "strain_Energy");
+        point_scalar_append2vtk(true, point_filename.c_str(), dat_str.vol_val_, dim, "vol_conservation_Energy");
+
+        vet_displace = displace.block(0, 0, 3, nods.cols());
+        writeOBJ(surf_filename.c_str(), (nods + vet_displace).transpose(), surf.transpose());
+
+      }
     }
-
-    dat_str.set_zero();
   }
   auto end = system_clock::now();
   auto duration = duration_cast<microseconds>(end - start);
