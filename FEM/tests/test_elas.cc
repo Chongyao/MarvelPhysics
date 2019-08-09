ed581e5e346291e3766b8bec8a26c60606fccd92
cyzhao <471038339@qq.com>
2019-08-09 13:51:57 +0800

    update

--- FEM/tests/test_elas.cc
+++ FEM/tests/test_elas.cc
@@ -15,7 +15,7 @@ using namespace Eigen;
 using namespace marvel;
 
 // using FLOAT_TYPE = double;
-using TET_ELAS = BaseElas<FLOAT_TYPE, 3, 4, 1, 1, arap_csttt, basis_func, quadrature>;
+using TET_ELAS = BaseElas<FLOAT_TYPE, 3, 4, 1, 1, linear_csttt, basis_func, quadrature>;
 int main(int argc, char** argv){
   Eigen::initParallel();
   std::cout.precision(17);
@@ -27,18 +27,18 @@ int main(int argc, char** argv){
   const size_t num_nods = nods.cols();
   cout <<"V"<< nods.rows() << " " << nods.cols() << endl << "T " << tets.rows() << " "<< tets.cols() << endl;
 
-  {//scale
-    nods.row(0) += Matrix<FLOAT_TYPE, 1, -1>::Ones(nods.cols()) * 10;
-    nods.row(1) += Matrix<FLOAT_TYPE, 1, -1>::Ones(nods.cols()) * 10;
-    nods.row(2) += Matrix<FLOAT_TYPE, 1, -1>::Ones(nods.cols()) * 0.5;
-    nods *= 0.05;
-  }
+  // {//scale
+  //   nods.row(0) += Matrix<FLOAT_TYPE, 1, -1>::Ones(nods.cols()) * 10;
+  //   nods.row(1) += Matrix<FLOAT_TYPE, 1, -1>::Ones(nods.cols()) * 10;
+  //   nods.row(2) += Matrix<FLOAT_TYPE, 1, -1>::Ones(nods.cols()) * 0.5;
+  //   nods *= 0.05;
+  // }
 
   const string outdir = argv[3];
   
   //set mtr
-  constexpr  FLOAT_TYPE rho = 100;
-  constexpr  FLOAT_TYPE Young = 5000.0;
+  constexpr  FLOAT_TYPE rho = 1;
+  constexpr  FLOAT_TYPE Young = 8000.0;
   constexpr  FLOAT_TYPE poi = 0.45;
   constexpr  FLOAT_TYPE gravity = 9.8;
   constexpr  FLOAT_TYPE dt = 0.01;
@@ -77,12 +77,12 @@ int main(int argc, char** argv){
   vector<shared_ptr<Functional<FLOAT_TYPE, 3>>> ebf(POS + 1);{
     ebf[ELAS] = make_shared<TET_ELAS>(nods, tets, Young, poi);
     // ebf[ELAS] = nullptr;
-    ebf[GRAV] = make_shared<gravity_energy<FLOAT_TYPE, 3>>(num_nods, 1, gravity, mass_vec, 'x');
+    ebf[GRAV] = make_shared<gravity_energy<FLOAT_TYPE, 3>>(num_nods, 1, gravity, mass_vec, 'y');
     ebf[KIN] = make_shared<momentum<FLOAT_TYPE, 3>>(nods.data(), num_nods, mass_vec, dt);
-    // ebf[POS] = make_shared<position_constraint<FLOAT_TYPE, 3>>(nods.data(), num_nods, w_pos, cons);
+    ebf[POS] = make_shared<position_constraint<FLOAT_TYPE, 3>>(nods.data(), num_nods, w_pos, cons);
 
     // ebf[POS] = make_shared<collision<FLOAT_TYPE, 3>>(nods.cols(), 1e5, 'x', 0.05, nods.cols(), init_points_ptr);
-    ebf[POS] = make_shared<geom_contact_energy<FLOAT_TYPE,3>>(objs, num_nods, 1e5);
+    // ebf[POS] = make_shared<geom_contact_energy<FLOAT_TYPE,3>>(objs, num_nods, 1e5);
     // ebf[POS] = nullptr;
     
     }
@@ -102,7 +102,7 @@ int main(int argc, char** argv){
     exit(EXIT_FAILURE);
   }
 
-  cout << "ehrer" << endl;
+
   //Sovle
   const string filename_tmp = outdir  + "/frame_origin.vtk";
   // tet_mesh_write_to_vtk<FLOAT_TYPE>(filename_tmp.c_str(), nods, tets);
