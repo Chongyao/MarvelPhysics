0d99e3cffe7e038c92b44320eb56c65a43951f7b
cyzhao <471038339@qq.com>
2019-09-17 09:34:04 +0800

    delete useless files

--- external/bigbang/readjson.h
+++
@@ -1,8 +0,0 @@
-#ifndef READJSON_H
-#define READJSON_H
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/json_parser.hpp>
-
-int read_cmd_by_json(const int &argc, const char* argv[], boost::property_tree::ptree &pt);
-
-#endif
